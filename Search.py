7e9c44881beb24abfe3d3da2de8368e2d7b16009
Chloe <31783292+chloeqino@users.noreply.github.com>
2022-11-16 23:40:40 -0800

    search class done

--- Search.py
+++ Search.py
@@ -8,6 +8,50 @@
 # Ranking: at the very least, your ranking formula should include tf-idf scoring, and take the important words into consideration, but you should feel free to add additional components to this formula if you think they improve the retrieval.
 
 # IMPORT OTHER CLASSES HERE
+import re
+import json
+class Search:
+
+    def readIndexOfIndex(self):
+        f = open("test_files/index_of_index.json")
+        self.tokenIndexJson = json.load(f)
+        f.close()
+    def tokenize(self):
+        pattern = "\s+"
+        return re.split(pattern,self.query)
+
+    def __init__(self, query):
+        self.query = query
+        self.indexfile = open("test_files/merged.txt","r")
+        tokens = self.tokenize();
+        print(tokens)
+        self.readIndexOfIndex()
+        self.results = dict()
+        for token in tokens:
+            if token in self.tokenIndexJson:
+                pos = self.tokenIndexJson[token]
+                self.indexfile.seek(pos)
+                jsonline = self.indexfile.readline()
+                print("jsonline",jsonline)
+                jsonline = json.loads(jsonline)
+                postings = jsonline[token]
+                newdict = dict()
+                if not bool(self.results):
+                    for docId in postings:
+                        newdict[docId] = {token:postings[docId]}
+                    self.results = newdict
+
+                else:
+                    for docId in postings:
+                        if docId in self.results:
+                            newdict[docId] = self.results[docId]
+                            newdict[docId][token] = postings[docId]
+                    self.results = newdict
+        print(self.results)
+
+
+
+
 
 if __name__ == '__main__':
-    pass
+    search = Search("apple aaa bbb")
