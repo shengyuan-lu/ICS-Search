f0c1a7717c4610b7f73a89ad1ec3ac3478d52817
Chloe <31783292+chloeqino@users.noreply.github.com>
2022-11-18 17:53:15 -0800

    Update Search.py

--- Search.py
+++ Search.py
@@ -10,52 +10,84 @@
 # IMPORT OTHER CLASSES HERE
 import re
 import json
+import os
+from nltk.stem import PorterStemmer
 class Search:
 
     def readIndexOfIndex(self):
-        f = open("test_files/index_of_index.json")
+        f = open("index_of_index.json")
         self.tokenIndexJson = json.load(f)
         f.close()
     def tokenize(self):
         pattern = "\s+"
         return re.split(pattern,self.query)
 
+    def readDocIdDict(self):
+        f = open("doc_id_dict.json")
+        self.idToUrl = json.load(f)
+        f.close()
+
+    def printResults(self,limit = 5):
+        count = 0
+
+        for r in self.sortedResults[0:limit+1]:
+            print(r[1]["url"])
+
+
+
+
+
+    def sortResults(self):
+        self.sortedResults = sorted(self.results.items(), key=lambda t: -t[1]["score"])
+
     def __init__(self, query):
         self.query = query
-        self.indexfile = open("test_files/merged.txt","r")
+        complete_name = os.path.join("Index", "final_index.txt")
+        self.indexfile = open(complete_name,"r")
         tokens = self.tokenize();
         print(tokens)
+        ps = PorterStemmer()
         # parse the index_of_index.json into a python dict
         # and save the result in tokenIndexJson
         self.readIndexOfIndex()
         self.results = dict()
+        self.readDocIdDict()
         for token in tokens:
-            if token in self.tokenIndexJson:
-                pos = self.tokenIndexJson[token]
+            stemmed_token = ps.stem(token)
+            if stemmed_token in self.tokenIndexJson:
+                pos = self.tokenIndexJson[stemmed_token]
                 self.indexfile.seek(pos)
                 jsonline = self.indexfile.readline()
-                print("jsonline",jsonline)
+
                 jsonline = json.loads(jsonline)
-                postings = jsonline[token]
+                postings = jsonline[stemmed_token]
                 newdict = dict()
 
                 # if the result dictionary is empty
                 if not bool(self.results):
                     for docId in postings:
+
                         newdict[docId] = {token:postings[docId]}
+                        newdict[docId]["url"] = self.idToUrl[docId]
+                        newdict[docId]["score"] = postings[docId]
                     self.results = newdict
                 # if not empty
                 else:
                     for docId in postings:
+                        #docId = self.idToUrl[docId]
                         if docId in self.results:
+
                             newdict[docId] = self.results[docId]
+                            newdict[docId]["url"] = self.idToUrl[docId]
                             newdict[docId][token] = postings[docId]
+                            newdict[docId]["score"] += postings[docId]
                     self.results = newdict
-        print(self.results)
+        self.sortResults()
+        self.printResults()
 
 
 
 
 
 if __name__ == '__main__':
-    search = Search("apple bbb")
+    search = Search("ics lab")
