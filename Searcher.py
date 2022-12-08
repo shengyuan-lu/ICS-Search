bae0948c88bcd35696ba89dac4c146a5caa64582
Chloe <31783292+chloeqino@users.noreply.github.com>
2022-12-08 02:28:10 -0800

    ready for deploy

--- Searcher.py
+++ Searcher.py
@@ -118,30 +118,3 @@ class Searcher:
         self.sorted_results = sorted(self.results.items(), key=lambda t: (len(t[1]['missing']), -t[1]['score']))
 
 
-@app.route('/search')
-def search():
-    start_time = time.time()
-
-
-    query = request.args.get('query')
-    query = query.strip()
-
-    if not query:
-        query = ''
-
-    print('Original Query: ' + query)
-
-    search_query = Searcher(query)
-    results = search_query.get_results(10)
-    end_time = time.time()
-
-    return render_template('index.html', results = results, process_time = (end_time-start_time)*1000, query = query)
-
-
-@app.route('/')
-def launch():
-    return render_template('main.html')
-
-
-if __name__ == '__main__':
-    app.run(port=8080, debug=True)
